fa88c5d0e507dd27761e7521d7e7887f5237dd12
AC Salazar <accsalazar2020@plm.edu.ph>
2023-11-28 22:34:05 +0800

    [Feature][Thea] Added presentation of text

---
+++ format_bitstuff.py
@@ -0,0 +1,15 @@
+import base64
+
+text2 = [
+    "100101000011100111",
+    "1100101000011100111",
+    "11100101000011111001",
+]
+
+encoded_messages = []
+for word in text2:
+    base64_encoded = base64.b64encode(word.encode("ascii"))
+    base64_message = base64_encoded.decode("ascii")
+    encoded_messages.append(base64_message)
+
+print(encoded_messages)
---
+++ format_destuff.py
@@ -0,0 +1,13 @@
+import base64
+import format_bitstuff
+
+encoded_messages = format_bitstuff.encoded_messages
+decoded_messages = []
+
+for encoded_message in encoded_messages:
+    base64_decoded = base64.b64decode(encoded_message)
+    decoded_message = base64_decoded.decode("ascii")
+    decoded_messages.append(decoded_message)
+
+# Printing decoded messages
+print("\nDecoded Messages:", decoded_messages)
