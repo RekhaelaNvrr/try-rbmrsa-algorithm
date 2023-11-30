96d2c95ff909a59e16fe4a65ac177a93fbb04d70
RekhaelaNvrr <navarro.rekhaela@gmail.com>
2023-11-30 16:06:34 +0800

    fixed files and folder, added checker for primes

---
+++ rbmrsa-mojisola/format_destuff.py
@@ -0,0 +1,15 @@
+import base64
+#import format_bitstuff
+
+
+
+def format_destuff(final_encoded_messages):
+    encoded_messages = final_encoded_messages.split("//")
+    decoded_messages = []
+    for encoded_message in encoded_messages:
+        base64_decoded = base64.b64decode(encoded_message)
+        decoded_message = base64_decoded.decode("ascii")
+        decoded_messages.append(decoded_message)
+
+    return decoded_messages
+    
